8e0577c2c4b6badff2a2eb7ac320faed8373fea2
Diogo Korok <korokd@protonmail.ch>
2020-08-23 00:32:20 -0300

    Make basic inputs inspired on Forms example

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
 
@@ -18,12 +19,17 @@ main =
 
 
 type alias Model =
-    Int
+    { housingPercentage : Float
+    , investingPercentage : Float
+    , leisurePercentage : Float
+    , housingUpperBound : Float
+    , housingLowerBound : Float
+    }
 
 
 init : Model
 init =
-    0
+    Model 0 0 0 0 0
 
 
 
@@ -31,18 +37,30 @@ init =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = HousingP String
+    | InvestingP String
+    | LeisureP String
+    | HousingUB String
+    | HousingLB String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        HousingP housingPercentage ->
+            { model | housingPercentage = housingPercentage |> String.toFloat |> Maybe.withDefault 0 }
 
-        Decrement ->
-            model - 1
+        InvestingP investingPercentage ->
+            { model | investingPercentage = investingPercentage |> String.toFloat |> Maybe.withDefault 0 }
+
+        LeisureP leisurePercentage ->
+            { model | leisurePercentage = leisurePercentage |> String.toFloat |> Maybe.withDefault 0 }
+
+        HousingUB housingUpperBound ->
+            { model | housingUpperBound = housingUpperBound |> String.toFloat |> Maybe.withDefault 0 }
+
+        HousingLB housingLowerBound ->
+            { model | housingLowerBound = housingLowerBound |> String.toFloat |> Maybe.withDefault 0 }
 
 
 
@@ -52,7 +70,24 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ viewInput "number" "Housing %" (String.fromFloat model.housingPercentage) HousingP
+        , viewInput "number" "Investing %" (String.fromFloat model.investingPercentage) InvestingP
+        , viewInput "number" "Leisure %" (String.fromFloat model.leisurePercentage) LeisureP
+        , viewInput "number" "Housing Min" (String.fromFloat model.housingLowerBound) HousingLB
+        , viewInput "number" "Housing Max" (String.fromFloat model.housingUpperBound) HousingUB
+        , validationMessage model
         ]
+
+
+viewInput : String -> String -> String -> (String -> Msg) -> Html Msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+validationMessage : Model -> Html Msg
+validationMessage model =
+    if model.housingPercentage + model.investingPercentage + model.leisurePercentage == 100 then
+        span [ style "color" "green" ] [ text "Nice" ]
+
+    else
+        span [ style "color" "red" ] [ text "Percentages must add to 100" ]
